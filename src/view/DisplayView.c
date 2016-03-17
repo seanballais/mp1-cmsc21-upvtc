be79818fee58fbbfcbee8e4f80bdfc990043506c
Sean Francis N. Ballais <sfballais123@gmail.com>
2016-03-17 20:45:26 +0800

    Initial rewrite.

---
+++ src/view/DisplayView.c
@@ -0,0 +1,22 @@
+/*
+ * DisplayView.c
+ *
+ * Contains the functions that handles any text display related
+ * tasks.
+ *
+ */
+
+#include "DisplayView.h"
+
+void
+DisplayView_printMenu(const unsigned int userRole)
+{
+    /*
+     * Prints a menu that displays the tasks that are available
+     * to a user privilege.
+     */
+
+    if (userRole == ADMIN) {
+
+    }
+}
---
+++ src/view/DisplayView.h
@@ -0,0 +1,20 @@
+/*
+ * DisplayView.h
+ *
+ * Contains the functions that handles any text display related
+ * tasks.
+ *
+ */
+
+#ifndef DISPLAYVIEW_H
+#define DISPLAYVIEW_H
+
+    #include <stdlib.h>
+
+    #define ADMIN 0
+    #define TEACHER 1
+    #define STUDENT 2
+
+    void DisplayView_printMenu(const unsigned int userRole);
+
+#endif
